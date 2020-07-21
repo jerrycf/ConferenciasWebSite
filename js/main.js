74b5cca083f53513f44983f3051c86b76cf45bff
jerrycf <superjerry95@gmail.com>
2020-07-21 13:30:48 -0500

    Agregado plugin de JQuery para el countdown y animaciones en los contadores y letras

--- js/main.js
+++ js/main.js
@@ -134,6 +134,10 @@
 
 
 $(function(){
+     //Lettering
+     $('.nombre-sitio').lettering();
+
+    //Programa de conferencias
     $('.programa-evento .info-curso:first').show();
     
     $('.menu-programa a:first').addClass('activo');
@@ -145,6 +149,20 @@ $(function(){
         $(enlace).fadeIn(1000);
 
         return false;
+    });
+
+    //Animaciones para los números
+    $(' .resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 3000);
+    $(' .resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 3000);
+    $(' .resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 3000);
+    $(' .resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 3000);
+
+    //Cuenta regresiva
+    $('.cuenta-regresiva').countdown('2020/10/30 09:35:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
     });
 });
 
